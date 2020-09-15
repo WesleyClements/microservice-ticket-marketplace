5b1d72ef58efba948c97f8184802be04b20dd9d1
Wesley Clements <wesley.clements@gmail.com>
2020-09-15 12:20:38 -0700

    added mock up for sign up route

--- auth/src/routes/api/users.ts
+++ auth/src/routes/api/users.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
@@ -6,10 +7,27 @@ router.get('/currentuser', (req, res) => {
   res.send('Hi there');
 });
 
-router.post('/signup', (req, res) => {});
+router.post(
+  '/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 6 }).withMessage('Password must be more than 6 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    console.log(`Creating user: ${email}, ${password}`, 'sdsdds');
+
+    res.status(201).json({});
+  }
+);
 
 router.post('/signin', (req, res) => {});
 
 router.post('/signout', (req, res) => {});
 
-export default router;
+export { router };
