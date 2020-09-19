b94fdf28fa818449e1aa236666104a605bb59e2d
Wesley Clements <wesley.clements@gmail.com>
2020-09-19 05:26:09 -0700

    added handler for system errors

--- auth/src/middleware/errorHandler/index.ts
+++ auth/src/middleware/errorHandler/index.ts
@@ -3,13 +3,17 @@ import { ErrorCollection } from './ErrorCollection';
 
 export { ErrorCollection };
 
-export const errorHandler = (err: ErrorCollection, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: ErrorCollection | Error, req: Request, res: Response, next: NextFunction) => {
   console.log('Something went wrong', err);
 
-  res.status(err.status);
+  if (err instanceof ErrorCollection) {
+    res.status(err.status);
 
-  res.send({
-    message: err.message,
-    errors: err.errors,
-  });
+    res.send({
+      message: err.message,
+      errors: err.errors,
+    });
+  } else {
+    res.status(500).send({ message: err.message });
+  }
 };
