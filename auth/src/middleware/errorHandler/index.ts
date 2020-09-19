1a32459e5f7c50b8c58e1c49a8f2e5c6e6308cd6
Wesley Clements <wesley.clements@gmail.com>
2020-09-19 13:29:12 -0700

    changed to only log message

--- auth/src/middleware/errorHandler/index.ts
+++ auth/src/middleware/errorHandler/index.ts
@@ -4,7 +4,8 @@ import { StandardizedError } from '@errors/StandardizedError';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof StandardizedError) {
     const { status, message, errors } = err;
-    res.status(status).send({ message, errors });
+    console.error(`Error: ${message}`);
+    res.status(status).send({ errors });
   } else {
     res.status(500).send({ message: err.message });
   }
