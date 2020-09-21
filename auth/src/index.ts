276a5769723b9efc9fabc8cdd4c27e20a26cb62d
Wesley Clements <wesley.clements@gmail.com>
2020-09-21 09:42:22 -0700

    added cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 
 import '@db';
 
@@ -9,7 +10,15 @@ const PORT = 3000;
 
 const app = express();
 
+app.set('trust proxy', true);
+
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use('/', require('./routes').router);
 
