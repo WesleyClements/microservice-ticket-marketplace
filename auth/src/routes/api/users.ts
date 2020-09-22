0c79c26990af70a2cf88a256c6be46d4f990e09c
Wesley Clements <wesley.clements@gmail.com>
2020-09-22 15:00:13 -0700

    changed to use validateRequest and added signin route

--- auth/src/routes/api/users.ts
+++ auth/src/routes/api/users.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
+import { validateRequest } from '@middleware';
+
 import { MongoError } from 'mongodb';
-import { BadRequestError, RequestValidationError } from '@errors';
+import { BadRequestError } from '@errors';
 
 import { User } from '@db';
 
@@ -16,14 +18,11 @@ router.get('/currentuser', (req, res) => {
 router.post(
   '/signup',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({ min: 6 }).withMessage('Password must be more than 6 characters'),
+    body('email').isEmail().withMessage('email must be valid'),
+    body('password').trim().isLength({ min: 6 }).withMessage('eassword must be more than 6 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
 
     try {
@@ -35,14 +34,39 @@ router.post(
       res.status(201).json(user);
     } catch (err) {
       if (err instanceof MongoError && err.code === 11000) {
-        throw new BadRequestError('Email in use');
+        throw new BadRequestError('email in use');
       }
-      throw new Error('Internal Error');
+      throw new Error('internal error');
     }
   }
 );
 
-router.post('/signin', (req, res) => {});
+router.post(
+  '/signin',
+  [
+    body('email').isEmail().withMessage('email must be valid'),
+    body('password').trim().notEmpty().withMessage('you must supply a password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    let user;
+    try {
+      user = await User.findOne({ email });
+    } catch (err) {
+      throw new Error('internal error');
+    }
+    if (user && (await user.comparePassword(password))) {
+      req.session = {
+        jwt: jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!),
+      };
+
+      res.status(200).json(user);
+    } else {
+      throw new BadRequestError('invalid credentials');
+    }
+  }
+);
 
 router.post('/signout', (req, res) => {});
 
