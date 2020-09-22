de81708cd4c38faf7af06f3545536e99c72f2925
Wesley Clements <wesley.clements@gmail.com>
2020-09-22 15:12:28 -0700

    added currentuser route

--- auth/src/routes/api/users.ts
+++ auth/src/routes/api/users.ts
@@ -12,6 +12,15 @@ import { User } from '@db';
 const router = Router();
 
 router.get('/currentuser', (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
   res.send('Hi there');
 });
 
