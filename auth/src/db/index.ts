eab5b526253cb56c32169857d57b239ed3318b11
Wesley Clements <wesley.clements@gmail.com>
2020-09-24 14:52:27 -0700

    changed to export connection logic as function

--- auth/src/db/index.ts
+++ auth/src/db/index.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 
-mongoose
-  .connect('mongodb://auth-mongo-srv:27017/auth', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('Connected to mongodb');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 export { User } from './models/User';
+
+export const connectToDB = () =>
+  mongoose
+    .connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log('Connected to mongodb');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
