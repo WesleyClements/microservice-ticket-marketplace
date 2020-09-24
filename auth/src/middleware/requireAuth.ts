f5c96b27050a031c8c58fe027bae427fd56d33df
Wesley Clements <wesley.clements@gmail.com>
2020-09-24 13:52:39 -0700

    added typing

--- auth/src/middleware/requireAuth.ts
+++ auth/src/middleware/requireAuth.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { UserRole } from '@db/models/User';
+import { UserRole } from 'db/models/User';
 
-import { AuthorizationError } from '@errors';
+import { AuthorizationError } from 'errors';
 
 export function requireAuth(...roles: UserRole[]): (req: Request, res: Response, next: NextFunction) => void;
 export function requireAuth(req: Request, res: Response, next: NextFunction): void;
 export function requireAuth(...args: any[]) {
   if (typeof args[0] === 'string') {
-    const roles = args;
+    const roles = args as UserRole[];
     return (req: Request, res: Response, next: NextFunction) => {
-      if (!roles.includes(req.currentUser?.role)) {
+      if (!req.currentUser || !roles.includes(req.currentUser.role)) {
         throw new AuthorizationError('unauthorized access');
       }
       next();
