4e711353595af3b4dbc6952d1e608be3d2da6c68
Wesley Clements <wesley.clements@gmail.com>
2020-09-24 13:04:04 -0700

    included AuthorizationError

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -1,3 +1,4 @@
 export { StandardizedError } from './StandardizedError';
 export { BadRequestError } from './BadRequestError';
+export { AuthorizationError } from './AuthorizationError';
 export { RequestValidationError } from './RequestValidationError';
