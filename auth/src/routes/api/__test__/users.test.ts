08785fea163d0f02e101983f7a68b1bba90d8d93
Wesley Clements <wesley.clements@gmail.com>
2020-09-25 12:19:27 -0700

    added tests for /api/users/signin

--- auth/src/routes/api/__test__/users.test.ts
+++ auth/src/routes/api/__test__/users.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest';
 import app from 'app';
 
-describe('/api/users/signup', () => {
+describe('/signup', () => {
   describe('POST', () => {
-    it('returns a 400 if missing email and/or password', async () => {
+    it('returns 400 if missing email and/or password', async () => {
       await Promise.all([
         request(app).post('/api/users/signup').send({}).expect(400),
         request(app)
@@ -17,21 +17,21 @@ describe('/api/users/signup', () => {
       ]);
     });
 
-    it('returns a 400 if invalid email', async () => {
+    it('returns 400 if invalid email', async () => {
       return request(app)
         .post('/api/users/signup')
         .send({ email: 'testtest.com', password: 'password' })
         .expect(400);
     });
 
-    it('returns a 400 if invalid password', async () => {
+    it('returns 400 if invalid password', async () => {
       return request(app)
         .post('/api/users/signup')
         .send({ email: 'test@test.com', password: 'pass' })
         .expect(400);
     });
 
-    it('returns a 400 if duplicate email', async () => {
+    it('returns 400 if duplicate email', async () => {
       await request(app)
         .post('/api/users/signup')
         .send({ email: 'test@test.com', password: 'password' })
@@ -42,7 +42,7 @@ describe('/api/users/signup', () => {
         .expect(400);
     });
 
-    it('returns a 201 if successful', async () => {
+    it('returns 201 if successful', async () => {
       return request(app)
         .post('/api/users/signup')
         .send({ email: 'test@test.com', password: 'password' })
@@ -54,6 +54,72 @@ describe('/api/users/signup', () => {
         .post('/api/users/signup')
         .send({ email: 'test@test.com', password: 'password' })
         .expect(201);
+      expect(res.get('Set-Cookie')).toBeDefined();
+    });
+  });
+});
+
+describe('/signin', () => {
+  describe('POST', () => {
+    it('returns 400 if missing email and/or password', async () => {
+      await Promise.all([
+        request(app).post('/api/users/signin').send({}).expect(400),
+        request(app)
+          .post('/api/users/signin')
+          .send({ email: 'test@test.com' })
+          .expect(400),
+        request(app)
+          .post('/api/users/signin')
+          .send({ password: 'password' })
+          .expect(400),
+      ]);
+    });
+
+    it('returns 400 if invalid email', async () => {
+      return request(app)
+        .post('/api/users/signin')
+        .send({ email: 'testtest.com', password: 'password' })
+        .expect(400);
+    });
+
+    it('returns 400 if unregistered email', async () => {
+      return request(app)
+        .post('/api/users/signin')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(400);
+    });
+
+    it('returns 400 if incorrect password', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+      return request(app)
+        .post('/api/users/signin')
+        .send({ email: 'test@test.com', password: 'pass' })
+        .expect(400);
+    });
+
+    it('returns 200 if valid credentials', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+      return request(app)
+        .post('/api/users/signin')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(200);
+    });
+
+    it('sets a cookie if valid credentials', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(201);
+      const res = await request(app)
+        .post('/api/users/signin')
+        .send({ email: 'test@test.com', password: 'password' })
+        .expect(200);
       expect(res.get('Set-Cookie')).toBeDefined();
     });
   });
