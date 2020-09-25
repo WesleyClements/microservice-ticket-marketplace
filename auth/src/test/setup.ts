c7a002ff6a2c21ed1a7ea815ba33ca8e3b9690fe
Wesley Clements <wesley.clements@gmail.com>
2020-09-25 00:42:49 -0700

    added setup for jest

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import app from 'app';
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  await Promise.all(collections.map((collection) => collection.deleteMany({})));
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.disconnect();
+});
