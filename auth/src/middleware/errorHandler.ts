e3769e10c481c9b8350b13d399affb0e9fe5051c
Wesley Clements <wesley.clements@gmail.com>
2020-09-26 11:27:54 -0700

    removed debug console log

--- auth/src/middleware/errorHandler.ts
+++ auth/src/middleware/errorHandler.ts
@@ -2,10 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 
 import { StandardizedError } from 'errors';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof StandardizedError) {
     const { status, message, errors } = err;
-    console.error(`Error: ${message}`);
     res.status(status).send({ errors });
   } else {
     res.status(500).send({ message: err.message });
