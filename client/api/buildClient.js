cf72a55b96707dc94d17b8e85c2d49b777df89f8
Wesley Clements <wesley.clements@gmail.com>
2020-10-10 08:03:12 -0700

    refactored to simplify

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const buildClient = ({req}) => {
-  if (typeof window === "undefined") {
-    return axios.create({
-      baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-      headers: req.headers
-    })
-  }
-  else {
-    return axios.create({
-      baseURL: "/"
-    })
-  }
-} 
+export const buildClient = ({ req }) => {
+  return axios.create(
+    typeof window === 'undefined'
+      ? {
+          baseURL:
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+          headers: req.headers,
+        }
+      : {
+          baseURL: '/',
+        }
+  );
+};
