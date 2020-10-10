030188e860366da33b5af9ace747a05d5e7f428d
Wesley Clements <wesley.clements@gmail.com>
2020-10-10 06:27:13 -0700

    added helper to set up axios instance for server/browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
-const Index = () => {
+import {buildClient} from "../api";
+
+const Index = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
+};
+
+Index.getInitialProps = async (context) => {
+  const axios = buildClient(context);
+  const res = await axios.get('/api/users/currentuser');
+  const { currentUser } = res.data;
+  return { currentUser };
 };
 
 export default Index;
