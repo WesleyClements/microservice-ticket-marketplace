0e226e6ca41e1a50245181945181ce5128b9ae74
Wesley Clements <wesley.clements@gmail.com>
2020-10-10 08:03:57 -0700

    changed to allow no arguments

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 
 const useRequest = ({ url, method }) => {
   const [errors, setErrors] = useState(null);
-  const doRequest = async ({ data, params }) => {
+  const doRequest = async (props) => {
+    let request = { method, url };
+    if (props) request = { ...props, ...request };
     try {
       setErrors(null);
-      return await axios({ method, url, data, params });
+      return await axios(request);
     } catch (err) {
       const errors = err?.response?.data?.errors;
       if (!errors) throw err;
