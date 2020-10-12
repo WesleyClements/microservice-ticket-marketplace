7fc0da46b791a7635996cc21f4caa5a08dab80a0
Wesley Clements <wesley.clements@gmail.com>
2020-10-12 09:55:49 -0700

    flushed out /api/tickets post tests

--- tickets/src/routes/api/__test__/tickets.test.ts
+++ tickets/src/routes/api/__test__/tickets.test.ts
@@ -4,11 +4,53 @@ import app from '../../../app';
 describe('/api/tickets', () => {
   describe('GET', () => {});
   describe('POST', () => {
-    it('listens for a post request', async () => {});
-    it('is only accessed by authenticated users', async () => {});
-    it('returns an error if invalid title', async () => {});
-    it('returns an error if invalid price', async () => {});
-    it('created a ticket with valid inputs', async () => {});
+    it('listens for a post request', async () => {
+      const res = await request(app).post('/api/tickets').send({});
+      expect(res.status).not.toEqual(404);
+    });
+    it('is only accessed by authenticated users', async () => {
+      await request(app).post('/api/tickets').send({}).expect(401);
+      const sessionCookie = global.createSessionCookie();
+      const res = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({});
+      expect(res.status).not.toEqual(401);
+    });
+    it('returns an error if invalid title', async () => {
+      const sessionCookie = global.createSessionCookie();
+      await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({})
+        .expect(400);
+      await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({ title: 1234 })
+        .expect(400);
+    });
+    it('returns an error if invalid price', async () => {
+      const sessionCookie = global.createSessionCookie();
+      await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({})
+        .expect(400);
+      await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({ price: 1234 })
+        .expect(400);
+    });
+    it('created a ticket with valid inputs', async () => {
+      const sessionCookie = global.createSessionCookie();
+      await request(app)
+        .post('/api/tickets')
+        .set('Cookie', sessionCookie)
+        .send({})
+        .expect(201);
+    });
   });
   describe('PUT', () => {});
 });
